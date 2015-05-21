712b0f586cf6717a287e210ffda40c8a3eabb735
hlxinyan@gmail.com <hlxinyan@gmail.com>
2015-05-21 19:17:11 +0800

    add all

--- client/app/js/DishController.js
+++ client/app/js/DishController.js
@@ -25,21 +25,50 @@
           var item=$scope.realOrderItems[i];
           if(item.dish.id==realOrder.dish.id){
              item.count -=1;
+            if(item.count ==0){
+              $scope.realOrderItems.splice(i,1);
+              $scope.deleteChecked(realOrder.dish);
+            }
+            break;
           }
-          if(item.count ==0){
-            $scope.realOrderItems.splice(i,1);
-          }
+
         }
       }
 
       $scope.addOne=function(realOrder){
         for (var i=0;i< $scope.realOrderItems.length;i++) {
           var item=$scope.realOrderItems[i];
+
           if(item.dish.id==realOrder.dish.id){
             item.count+=1;
+            break;
 
           }
 
+        }
+      }
+
+      $scope.deleteOrder=function(realOrder){
+
+           console.log(realOrder);
+
+        for (var i=0;i< $scope.realOrderItems.length;i++) {
+          var item=$scope.realOrderItems[i];
+          console.log(item);
+          if(item.dish.id==realOrder.dish.id){
+            $scope.realOrderItems.splice(i,1);
+            $scope.deleteChecked(realOrder.dish);
+          }
+
+        }
+      }
+
+      $scope.deleteChecked=function(dish){
+        for(var i=0;i< $scope.selectedOrderItems.length;i++){
+          var item=$scope.selectedOrderItems[i];
+          if(item.id==dish.id){
+            $scope.selectedOrderItems.splice(i,1);
+          }
         }
       }
 
